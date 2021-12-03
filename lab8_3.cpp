9c6557e4487002caa399e43a3944c1e2d8884741
pulom999 <pulom999@gmail.com>
2021-12-03 15:24:51 +0700

    เสร็จ

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char X){
+	if(X == 65){
+		return X = 'Z';		
+	}
+	if(X >= 65 && X <=90){
+		X = X-1 ;
+	}else{
+		return '0';
+	}
+	return X;
 }
 
 int main(){
